f810210e4bd6f0aa1f7c8087b82a0e8361ed0629
gaaamii <i1shi4ga4mi1@gmail.com>
2018-02-05 00:48:53 +0900

    times 2 してから times 3 するやつその2

--- tests/Tests.elm
+++ tests/Tests.elm
@@ -27,7 +27,7 @@ suite =
                     Expect.equal subject { amount = 10 }
             ]
         , describe "times 2 してから times 3 するやつ"
-            [ test "$5 * 2 = $10, $5 * 3 = 15" <|
+            [ test "$5 * 3 = 15" <|
                 \_ ->
                     let
                         times_3 =
@@ -36,6 +36,23 @@ suite =
                                     Dollar 5 |> Dollar.times 2 |> Dollar.amount
                             in
                             Dollar 5 |> Dollar.times 3 |> Dollar.amount
+                    in
+                    Expect.equal times_3 15
+            ]
+        , describe "times 2 してから times 3 するやつその2"
+            [ test "$5 * 3 = 15" <|
+                \_ ->
+                    let
+                        times_3 =
+                            let
+                                times_2 =
+                                    let
+                                        five =
+                                            Dollar 5
+                                    in
+                                    five |> Dollar.times 2
+                            in
+                            times_2 |> Dollar.times 3 |> Dollar.amount
                     in
                     Expect.equal times_3 15
             ]
